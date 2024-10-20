4f9656a5d199f6bb6721b1beaab3d5a8cba143d8
Giorgia Barberia Gorrieri <giorgiabarberiagorrieri@giorgias-air.lan>
2024-10-20 16:33:23 -0300

    mensaje

--- class_mensajeria.py
+++ class_mensajeria.py
@@ -184,6 +184,7 @@ class Email():
             
     # Función para mostrar todos los emails por fecha (del último al primero)
     def mostrar_emails_por_fecha(self):
+<<<<<<< HEAD
         print('\nFiltro: Por Fecha.')
         self.bandeja_entrada = sorted(self.bandeja_entrada, key=lambda e: e['Fecha'], reverse = True)
         for i,email in enumerate(self.bandeja_entrada, start = 1):
@@ -242,3 +243,12 @@ class Email():
                 break
             else:
                 print('Opción inválida. Intente nuevamente.')
+=======
+        if not self.bandeja_entrada:
+            print('Bandeja de entrada vacía.')
+        else:
+            print('Emails en bandeja de entrada (ordenados por fecha):')
+            for mensaje in reversed(self.bandeja_entrada):
+                print(f'Email de {mensaje["Remitente"]} - {mensaje["Hora"]}: {mensaje["Contenido"]}')
+#hola
+>>>>>>> 7f6f155 (Descripción de los cambios locales/usr/local/bin/python3)
